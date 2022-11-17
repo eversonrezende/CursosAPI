e93539805b04297e8499e3446770424c74d72b54
erezendeosg <erezende@osg.com.br>
2022-11-17 15:57:23 -0300

    projeto inicial com método get e post

---
+++ CursosAPI/Controllers/VideoController.cs
@@ -0,0 +1,40 @@
+﻿using CursosAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideosController : ControllerBase
+    {
+        private static List<Video> videos = new List<Video>();
+        private static int id = 1;
+
+        //conforme padrão arquitetural REST, deve retornar uma action e onde o conteúdo foi criado
+        [HttpPost]
+        public IActionResult AdicionaVideo([FromBody] Video video)
+        {
+            video.Id = id++;
+            videos.Add(video);
+            return CreatedAtAction(nameof(RecuperaVideoPorId), new { Id = video.Id }, video);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaVideos()
+        {
+            return Ok(videos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaVideoPorId(int id)
+        {
+            Video video = videos.FirstOrDefault(video => video.Id == id);
+
+            if(video != null)
+            {
+                return Ok(video);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ CursosAPI/Models/Video.cs
@@ -0,0 +1,11 @@
+﻿namespace CursosAPI.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string URL { get; set; }
+        
+    }
+}
