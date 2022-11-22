5e56c68cf38e4f5a6f4620601b0871b4db06c6ec
erezendeosg <erezende@osg.com.br>
2022-11-22 08:50:03 -0300

    cria tabela categoria

--- CursosAPI/Program.cs
+++ CursosAPI/Program.cs
@@ -12,6 +12,7 @@ namespace CursosAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<CategoriaContext>(opts => opts.UseSqlServer("Server=localhost,1433;Database=curso;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True"));
             builder.Services.AddDbContext<VideoContext>(opts => opts.UseSqlServer("Server=localhost,1433;Database=curso;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True"));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
